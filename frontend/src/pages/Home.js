baadda6ef494f511aa2a1f48cfd71abf9f324676
zhiqingw <zhiqingw@student.unimelb.edu.au>
2022-04-30 13:32:12 +0800

    feat: nav

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
 export default function Home(){
-    
+
     return(
+        <div>
+            <header>
+                <div class="navbar-nav">
+                    <li class="active"><a href="/">Home</a></li>
+                    <li><a href="/f1">Overview</a></li>
+                </div>
+            </header>
         <h1>
             home
         </h1>
+        </div>
 
     )
 }
--- frontend/src/pages/Second.js
+++ frontend/src/pages/Second.js
@@ -2,9 +2,17 @@ import React from "react";
 export default function Second(){
     
     return(
-        <h1>
-            Second
-        </h1>
+        <div>
+            <header>
+                <div class="navbar-nav">
+                    <li><a href="/">Home</a></li>
+                    <li class="active"><a href="/f1">Overview</a></li>
+                </div>
+            </header>
+            <h1>
+                Second
+            </h1>
+        </div>
 
     )
 }
